fa29b2f8cf8ed720cda60d6e5bed3cd1c218f4fe
tahsin-99 <somethinglearn53@gmail.com>
2025-11-10 23:03:59 +0600

    navbar-done

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -7,19 +7,19 @@ const Banner = () => {
   const data = [
     {
       id: 1,
-      coverPhoto: "/images/pet1.jpg",
+      coverPhoto: "https://i.ibb.co.com/BVxJjNxT/pets.jpg",
       title: "Find Your Furry Friend!",
       subtitle: "Adopt, Don’t Shop — Give a Pet a Home."
     },
     {
       id: 2,
-      coverPhoto: "/images/pet2.jpg",
+      coverPhoto: "https://i.ibb.co.com/BVhHnK9V/food.jpg",
       title: "Because Every Pet Deserves Love",
       subtitle: "Bring happiness home today!"
     },
     {
       id: 3,
-      coverPhoto: "/images/pet3.jpg",
+      coverPhoto: "https://i.ibb.co.com/N2ybkftQ/withowners.jpg",
       title: "Adopt a Pet, Save a Life",
       subtitle: "Join our mission to care for animals."
     }
